3afc79e148b29c64eccc283f49e781064b397c3c
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-10 14:27:05 +0900

    create homeView

--- CockDive/CockDive/View/MainView.swift
+++ CockDive/CockDive/View/MainView.swift
@@ -3,8 +3,13 @@ import SwiftUI
 struct MainView: View {
     @State private var viewSelection: ViewType = .cockCard
     var body: some View {
-        TabView {
-            
+        TabView(selection: $viewSelection) {
+            CockCardsView()
+                .tabItem {
+                    Image(systemName: "fork.knife")
+                }
+                .tag(ViewType.cockCard)
+            Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
         }
     }
 }
