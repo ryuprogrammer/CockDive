5cded6ee7f666db6c3d03ea862d60fdbf147c630
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-11 20:11:33 +0900

    modified AddCockView

--- CockDive/CockDive/View/AddCockView.swift
+++ CockDive/CockDive/View/AddCockView.swift
@@ -1,13 +1,41 @@
 import SwiftUI
 
 struct AddCockView: View {
+    
+    // 画面サイズ取得
+    let window = UIApplication.shared.connectedScenes.first as? UIWindowScene
+    
     var body: some View {
         NavigationStack {
-            ScrollView {
-                Text("写真")
+            ZStack {
+                List {
+                    Section {
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("写真を追加")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundStyle(Color.white)
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 300)
+                                .background(Color.black)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                        })
+                    } header: {
+                        Text("①写真: まずは写真を追加しよう")
+                    }
+                    .listRowSeparator(.hidden)
+                }
+                .listStyle(.plain)
                 
-                RoundedRectangle(cornerRadius: 10)
-                    .frame(width: 100)
+                VStack {
+                    Spacer()
+                    // 投稿ボタン
+                    LongBarButton(text: "投稿する") {
+                        
+                    }
+                }
             }
             .navigationTitle("ご飯を投稿")
             .navigationBarTitleDisplayMode(.inline)
