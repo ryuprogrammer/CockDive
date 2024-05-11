c1cfe44f7df54ce9b5db22063fb036ac78950dca
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-11 19:52:43 +0900

    create ButtonUI

--- CockDive/CockDive/View/AddCockView.swift
+++ CockDive/CockDive/View/AddCockView.swift
@@ -3,8 +3,11 @@ import SwiftUI
 struct AddCockView: View {
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("追加画面")
+            ScrollView {
+                Text("写真")
+                
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 100)
             }
             .navigationTitle("ご飯を投稿")
             .navigationBarTitleDisplayMode(.inline)
--- CockDive/CockDive/ViewComponent/LongBarButton.swift
+++ CockDive/CockDive/ViewComponent/LongBarButton.swift
@@ -1,26 +1,44 @@
 import SwiftUI
 
+/// 横長の丸みを帯びたボタン
 struct LongBarButton: View {
-    @State var text: String = "ボタン"
+    // ボタンに表示する文言
+    let text: String
+    // タップ処理
+    let action: () -> Void
     
+    // 画面サイズ取得
     let window = UIApplication.shared.connectedScenes.first as? UIWindowScene
+    
+    init(text: String, action: @escaping () -> Void) {
+        self.text = text
+        self.action = action
+    }
     
     var body: some View {
         Button(action: {
-            
+            action()
         }, label: {
             Text(text)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(Color.white)
                 .frame(
-                    width: window?.screen.bounds.width,
-                    height: (window?.screen.bounds.height ?? 400) / 10
+                    width: (window?.screen.bounds.width ?? 400) - 50,
+                    height: (window?.screen.bounds.height ?? 800) / 15
                 )
-                .padding()
                 .background(Color.mainColor)
-                .clipShape(RoundedRectangle(cornerRadius: 15))
+                .clipShape(RoundedRectangle(cornerRadius: 100))
+                .padding()
         })
     }
 }
 
 #Preview {
-    LongBarButton()
+    LongBarButton(
+        text: "ボタン",
+        action: {
+            // 何もしない
+        }
+    )
 }
