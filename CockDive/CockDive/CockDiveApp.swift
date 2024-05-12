307dcec8a8d4c5c4a902dad257516f8f50643633
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-13 01:39:21 +0900

    change TabView

--- CockDive/CockDive/CockDiveApp.swift
+++ CockDive/CockDive/CockDiveApp.swift
@@ -4,10 +4,31 @@ import SwiftUI
 struct CockDiveApp: App {
     let persistenceController = PersistenceController.shared
 
+    @State private var viewSelection: ViewType = .cockCard
+    
     var body: some Scene {
         WindowGroup {
-            MainView()
+            TabView(selection: $viewSelection) {
+                CockCardsView()
+                    .tabItem {
+                        Image(systemName: "fork.knife")
+                        Text("ご飯")
+                    }
+                    .tag(ViewType.cockCard)
+                
+                MyPageView()
+                    .tabItem {
+                        
+                        Image(systemName: "house")
+                        Text("ホーム")
+                    }.tag(ViewType.home)
+            }
 //                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
+}
+
+enum ViewType {
+    case cockCard
+    case home
 }
