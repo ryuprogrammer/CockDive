96ba8926fa3451072b8c1aa4e5953426dc7d76ff
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-13 17:49:43 +0900

    create commentView

---
+++ CockDive/CockDive/View/CockPostViews/PostDetailView/PostCommentView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct PostCommentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostCommentView()
+}
