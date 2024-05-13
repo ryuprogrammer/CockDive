dd994afcc491ee69e7981af45e967596f39036f9
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-13 18:49:56 +0900

    add commentView

--- CockDive/CockDive/View/CockPostViews/PostDetailView/PostCommentView.swift
+++ CockDive/CockDive/View/CockPostViews/PostDetailView/PostCommentView.swift
@@ -1,8 +1,40 @@
 import SwiftUI
 
 struct PostCommentView: View {
+    @State private var comments: [String] = [
+        "美味しそう！",
+        "レシピを教えてください！",
+        "最長文字数に挑戦中最長文字数に挑戦中最長文字数に挑戦中最長文字数に挑戦中最長文字数に挑戦中最長文字数に挑戦中",
+        "お腹すいた"
+    ]
+    
+    // 画面サイズ取得
+    let window = UIApplication.shared.connectedScenes.first as? UIWindowScene
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ForEach(comments, id: \.self) { comment in
+            HStack(alignment: .top) {
+                Image("cockImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(
+                        width: (window?.screen.bounds.width ?? 50) / 10,
+                        height: (window?.screen.bounds.width ?? 50) / 10
+                    )
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("名前")
+                        
+                    }
+                    Text(comment)
+                }
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
--- CockDive/CockDive/View/CockPostViews/PostDetailView/PostDetailView.swift
+++ CockDive/CockDive/View/CockPostViews/PostDetailView/PostDetailView.swift
@@ -1,8 +1,79 @@
 import SwiftUI
 
 struct PostDetailView: View {
+    @State private var userName: String = "momo"
+    @State private var title: String = "定食"
+    @State private var explain: String = """
+ここに説明文を挿入ここに説明文を挿入ここに説明文を挿入ここに説明文を挿入ここに説明文を挿入
+"""
+    
+    @State private var isLike: Bool = false
+    @State private var likeCount: Int = 200
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            VStack {
+                HStack(alignment: .bottom) {
+                    Text(title)
+                        .font(.title)
+                    Text("\(userName)さん")
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("フォロー")
+                            .font(.callout)
+                            .padding(.horizontal, 3)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(Color.blue, lineWidth: 1.5)
+                            )
+                    })
+                    Spacer()
+                    
+                    Image(systemName: "ellipsis")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20)
+                        .foregroundStyle(Color.primary)
+                }
+                
+                ZStack {
+                    Image("cockImage")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(height: 250)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                }
+                
+                Text(explain)
+                    .font(.callout)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+        .listStyle(.plain)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbarBackground(Color("main"), for: .navigationBar)
+        .toolbarBackground(.visible, for: .navigationBar)
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                Text(title)
+                    .foregroundStyle(Color.white)
+                    .fontWeight(.bold)
+                    .font(.title3)
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "ellipsis")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 35)
+                        .foregroundStyle(Color.white)
+                })
+            }
+        }
     }
 }
 
