ef5b64d36266580fab114cff128d411b25fe8758
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-14 12:57:01 +0900

    create FirstView

---
+++ CockDive/CockDive/View/FirstView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstView()
+}
