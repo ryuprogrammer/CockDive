926340aca8f2821ecd2e1bb622a450e42abb94d0
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-15 13:27:52 +0900

    create CommentElemet

---
+++ CockDive/CockDive/Model/Element/CommentElement.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct CommentElement {
+    var id: String
+    /// uid
+    var uid: String
+    /// コメント内容
+    var comment: String
+    /// 投稿日
+    var createAt: Date
+}
---
+++ CockDive/CockDive/Model/Element/PostElement.swift
@@ -0,0 +1,8 @@
+//
+//  PostElement.swift
+//  CockDive
+//
+//  Created by トム・クルーズ on 2024/05/15.
+//
+
+import Foundation
