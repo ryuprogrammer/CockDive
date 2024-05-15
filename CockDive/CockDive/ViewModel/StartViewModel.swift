f806dfcee5b38f9af0331bbdfca2f1741f20e8f5
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-16 00:26:37 +0900

    change StartViewModel

--- CockDive/CockDive/ViewModel/StartViewModel.swift
+++ CockDive/CockDive/ViewModel/StartViewModel.swift
@@ -7,6 +7,7 @@ class StartViewModel: ObservableObject {
     private var handle: AuthStateDidChangeListenerHandle!
     
     private let userDataModel = UserDataModel()
+    private let userDefaultsDataModel = UserDefaultsDataModel()
     
     init() {
         // 認証状態の変化を監視するリスナー
@@ -35,14 +36,16 @@ class StartViewModel: ObservableObject {
         }
     }
     
-    /// 登録終わって、Statusを通常ユーザーに変更
-    func register() {
-        self.userStatus = .normalUser
-    }
-    
     /// Userの追加
     func addUser(nickName: String) async {
         await userDataModel.addUser(user: UserElement(nickName: nickName))
+        if let uid = userDataModel.fetchUid() {
+            userDefaultsDataModel.addUserData(user: UserElement(
+                id: uid,
+                nickName: nickName
+            ))
+        }
+        
         DispatchQueue.main.async {
             // Publishで変更→UI処理なので、メインスレッドで処理する
             self.userStatus = .normalUser
