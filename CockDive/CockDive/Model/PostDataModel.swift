9b2ac495202c5942d1b309c7ec6d24011bc0a367
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-17 00:24:23 +0900

    fix code

--- CockDive/CockDive/Model/PostDataModel.swift
+++ CockDive/CockDive/Model/PostDataModel.swift
@@ -74,23 +74,23 @@ struct PostDataModel {
     
     /// Postを取得（件数指定）
     func fetchPostData() async -> [PostElement] {
-            let docRef = db.collection(postDataCollection)
-                .order(by: "createAt", descending: true)
-                .limit(to: fetchPostLimit)
-            var postData: [PostElement] = []
-            
-            do {
-                let querySnapshot = try await docRef.getDocuments()
-                for document in querySnapshot.documents {
-                    let result = try document.data(as: PostElement.self)
-                    postData.append(result)
-                }
-            } catch {
-                print("Error getting documents: \(error)")
+        let docRef = db.collection(postDataCollection)
+            .order(by: "createAt", descending: true)
+            .limit(to: fetchPostLimit)
+        var postData: [PostElement] = []
+        
+        do {
+            let querySnapshot = try await docRef.getDocuments()
+            for document in querySnapshot.documents {
+                let result = try document.data(as: PostElement.self)
+                postData.append(result)
             }
-            
-            return postData
+        } catch {
+            print("Error getting documents: \(error)")
         }
+        
+        return postData
+    }
     
     // TODO: データ取得できるか確認 - uidとcreateAtが違うからエラーかな、、、
     /// Postを取得（Uid/ 件数指定）
