b6aa8b4b6f81ae6afbd4f397f7cb432b71438f60
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-19 20:06:22 +0900

    add deleteCommtn

--- CockDive/CockDive/ViewModel/PostDetailViewModel.swift
+++ CockDive/CockDive/ViewModel/PostDetailViewModel.swift
@@ -10,8 +10,19 @@ class PostDetailViewModel: ObservableObject {
     
     // MARK: - データ追加
     /// コメント追加
-    func addCommet(post: PostElement, newComment: CommentElement) {
+    func addCommet(post: PostElement, comment: CommentElement) async {
+        var newComment = comment
+        let uid = fetchUid()
         
+        if let userData = await userDataModel.fetchUserData(uid: uid) {
+            newComment.commentUserNickName = userData.nickName
+            newComment.commentUserIconURL = userData.iconURL
+            await commentDataModel.addComment(post: post, comment: newComment)
+        }
+    }
+    
+    func deleteComment(post: PostElement, comment: CommentElement) async {
+        await commentDataModel.deleteComment(post: post, deleteComment: comment)
     }
     
     /// Like
@@ -35,6 +46,11 @@ class PostDetailViewModel: ObservableObject {
     }
     
     // MARK: - データ取得
+    /// uid取得
+    func fetchUid() -> String {
+        return postDataModel.fetchUid() ?? ""
+    }
+    
     /// フォローしているか判定
     func isFollowFriend(friendUid: String) async {
         guard let uid = userDataModel.fetchUid() else { return }
