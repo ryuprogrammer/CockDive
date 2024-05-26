3429df14df0baa7c7ca53b05929142029a26c3b6
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-27 01:02:33 +0900

    fix error

--- CockDive/CockDive/ViewModel/MyPageViewModel.swift
+++ CockDive/CockDive/ViewModel/MyPageViewModel.swift
@@ -90,6 +90,7 @@ class MyPageViewModel: ObservableObject {
                     self.loadStatus = .completion
                 }
             case .failure(let error):
+                print("fetchMorePosts: error: \(error)")
                 // エラーが発生した場合の処理
                 DispatchQueue.main.async {
                     self.loadStatus = .error
