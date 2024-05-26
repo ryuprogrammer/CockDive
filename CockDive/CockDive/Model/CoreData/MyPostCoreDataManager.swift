f07e081f1a82586b33af6c320ac6a8b42f80349d
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-26 12:24:09 +0900

    create clender func

--- CockDive/CockDive/Model/CoreData/MyPostCoreDataManager.swift
+++ CockDive/CockDive/Model/CoreData/MyPostCoreDataManager.swift
@@ -22,6 +22,39 @@ class MyPostCoreDataManager {
         }
     }
 
+    /// 指定された月のMyPostModelを取得する
+    /// - Parameter date: 検索する月を含む日付
+    /// - Returns: 指定された月に作成されたMyPostModelとその日のタプルの配列
+    func fetchByMonth(date: Date) -> [(day: Int, post: MyPostModel)] {
+        let request: NSFetchRequest<MyPostModel> = MyPostModel.fetchRequest()
+
+        // Calendarを使って指定された月の開始日を取得
+        let calendar = Calendar.current
+        let startOfMonth = calendar.date(from: calendar.dateComponents([.year, .month], from: date))!
+
+        // 指定された月の日数を取得
+        let range = calendar.range(of: .day, in: .month, for: startOfMonth)!
+
+        // 月の最終日を計算
+        let endOfMonth = calendar.date(byAdding: .day, value: range.count - 1, to: startOfMonth)!
+
+        // フェッチリクエストに使用する述語を作成
+        let predicate = NSPredicate(format: "createAt >= %@ AND createAt < %@", startOfMonth as NSDate, endOfMonth as NSDate)
+        request.predicate = predicate
+
+        do {
+            // データをフェッチ
+            let results = try context.fetch(request)
+
+            // フェッチした結果をタプルの配列に変換し、返す
+            return results.map { (day: calendar.component(.day, from: $0.createAt ?? Date()), post: $0) }
+        } catch {
+            // エラーハンドリング
+            print("Failed to fetch MyPostModel for the month: \(error)")
+            return []
+        }
+    }
+
     // MARK: - データの追加
 
     /// 新しいMyPostModelを作成する
--- CockDive/CockDive/ViewModel/MyPageViewModel.swift
+++ CockDive/CockDive/ViewModel/MyPageViewModel.swift
@@ -1,6 +1,30 @@
 import Foundation
 
 class MyPageViewModel: ObservableObject {
-    
-    let coreDataMyDataModel = MyDataCoreDataManager.shared
+    /// UserDefaultsの基本データ（ニックネーム、自己紹介、アイコン）
+    @Published var userData: UserElementForUserDefaults? = nil
+    /// UserFriendElement（フォロー、フォロワー）
+    @Published var friendData: UserFriendElement? = nil
+    /// 投稿データ
+    @Published var myPostData: [(day: Int, post: MyPostModel)] = []
+
+    let userDefaultsDataModel = UserDefaultsDataModel()
+    let userFriendModel = UserFriendModel()
+    let myPostManager = MyPostCoreDataManager.shared
+
+    // MARK: - データ取得
+    /// 基本データ取得（ニックネーム、自己紹介、アイコン）
+    func fetchUserData() {
+        userData = userDefaultsDataModel.fetchUserData()
+    }
+
+    /// UserFriendElement取得（フォロー、フォロワー）
+    func fetchUserFriendElement(uid: String) async {
+        friendData = await userFriendModel.fetchUserFriendData(uid: uid)
+    }
+
+    /// 投稿データ取得
+    func fetchMyPostData(date: Date) {
+        myPostData = myPostManager.fetchByMonth(date: date)
+    }
 }
