6ab6c9aaca51e2c71c3e838a141ec450c0bc6779
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-28 20:11:10 +0900

    create MyProfileVM

---
+++ CockDive/CockDive/ViewModel/MyProfileEditViewModel.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+class MyProfileEditViewModel: ObservableObject {
+    private let userDataModel = UserDataModel()
+    private let userDefaultsDataModel = UserDefaultsDataModel()
+
+    /// Userの登録
+    func upDateUserData(
+        nickName: String,
+        introduction: String?,
+        iconImage: Data?
+    ) async {
+        // Firebaseに追加
+        await userDataModel.addUser(user: UserElement(
+            nickName: nickName,
+            introduction: introduction,
+            iconImage: iconImage
+        ))
+        // uidを取得して、UserDefaultsに追加
+        if let uid = userDataModel.fetchUid() {
+            userDefaultsDataModel.addUserData(user: UserElement(
+                id: uid,
+                nickName: nickName,
+                introduction: introduction,
+                iconImage: iconImage
+            ))
+        }
+    }
+}
