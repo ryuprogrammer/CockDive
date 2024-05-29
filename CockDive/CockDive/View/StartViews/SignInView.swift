a878ea7cea23338ac9f56b7c65fbda9e38be3473
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-30 01:09:02 +0900

    fix UI

--- CockDive/CockDive/View/StartViews/SignInView.swift
+++ CockDive/CockDive/View/StartViews/SignInView.swift
@@ -2,7 +2,16 @@ import SwiftUI
 
 struct SignInView: View {
     @State private var isShowSheet = false
-    
+    @State private var showViewType: ShowViewType? = nil
+    @State private var isLoading = true
+
+    private enum ShowViewType: Identifiable {
+        case privacyPolicyView
+        case termsOfServiceView
+
+        var id: Self { self }
+    }
+
     var body: some View {
         ZStack {
             Color.mainColor
@@ -20,7 +29,25 @@ struct SignInView: View {
                 LongBarButton(text: "サインイン", isStroke: true) {
                     self.isShowSheet.toggle()
                 }
-                
+
+                HStack {
+                    Button {
+                        showViewType = .termsOfServiceView
+                    } label: {
+                        Text("利用規約")
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.white)
+                    }
+
+                    Button {
+                        showViewType = .privacyPolicyView
+                    } label: {
+                        Text("プライバシーポリシー")
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.white)
+                    }
+                }
+
                 Spacer()
                     .frame(height: 100)
             }
@@ -28,6 +55,14 @@ struct SignInView: View {
         .ignoresSafeArea(.all)
         .sheet(isPresented: $isShowSheet) {
             FirebaseAuthUIView()
+        }
+        .sheet(item: $showViewType) { type in
+            switch type {
+            case .privacyPolicyView:
+                PrivacyPolicyView(path: .constant([]))
+            case .termsOfServiceView:
+                TermsOfServiceView(path: .constant([]))
+            }
         }
     }
 }
