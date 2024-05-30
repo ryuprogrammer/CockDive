dc9b7ad9c113975a29215d6d152f36417b392628
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-30 13:47:22 +0900

    fix block

--- CockDive/CockDive/ViewModel/AuthenticationManager.swift
+++ CockDive/CockDive/ViewModel/AuthenticationManager.swift
@@ -3,11 +3,11 @@ import FirebaseAuth
 import Combine
 
 class AuthenticationManager: ObservableObject {
-    // iOS16でも使うので、Publuish
+    // iOS16でも使うので、Publish
     @Published private(set) var isSignIn: Bool = false
     @Published private(set) var userStatus: UserStatus = .signInRequired
     private var handle: AuthStateDidChangeListenerHandle!
-    
+
     init() {
         // 認証状態の変化を監視するリスナー
         handle = Auth.auth().addStateDidChangeListener { [weak self] (auth, user) in
@@ -23,12 +23,12 @@ class AuthenticationManager: ObservableObject {
             }
         }
     }
-    
+
     deinit {
         // 認証状態の変化の監視を解除する
         Auth.auth().removeStateDidChangeListener(handle)
     }
-    
+
     func signOut() {
         do {
             try Auth.auth().signOut()
@@ -36,10 +36,29 @@ class AuthenticationManager: ObservableObject {
             print("Error signing out: \(error.localizedDescription)")
         }
     }
-    
+
     /// 登録終わって、Statusを通常ユーザーに変更
     func register() {
         self.userStatus = .normalUser
     }
-}
 
+    /// アカウントを削除するメソッド
+    func deleteAccount(completion: @escaping (Result<Void, Error>) -> Void) {
+        guard let user = Auth.auth().currentUser else {
+            completion(.failure(NSError(domain: "AuthError", code: -1, userInfo: [NSLocalizedDescriptionKey: "No user is signed in."])))
+            return
+        }
+
+        user.delete { error in
+            if let error = error {
+                print("Error deleting account: \(error.localizedDescription)")
+                completion(.failure(error))
+            } else {
+                print("Account deleted successfully")
+                self.isSignIn = false
+                self.userStatus = .signInRequired
+                completion(.success(()))
+            }
+        }
+    }
+}
