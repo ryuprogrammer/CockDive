eeeb8a6924310fd46fe1aa675179ddb5e8286f21
トム・クルーズ <ryu1421magic@icloud.com>
2024-06-06 00:37:32 +0900

    fix

---
+++ CockDive/CockDive/View/CockPostViews/PostEditView.swift
@@ -0,0 +1,18 @@
+//
+//  PostEditView.swift
+//  CockDive
+//
+//  Created by トム・クルーズ on 2024/06/05.
+//
+
+import SwiftUI
+
+struct PostEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostEditView()
+}
---
+++ CockDive/CockDive/ViewModel/PostEditViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostEditViewModel.swift
+//  CockDive
+//
+//  Created by トム・クルーズ on 2024/06/05.
+//
+
+import Foundation
