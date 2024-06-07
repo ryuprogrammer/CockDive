30c1603a3c1f6099b2b5bf84f51a7af8dfce6827
トム・クルーズ <ryu1421magic@icloud.com>
2024-06-07 18:46:21 +0900

    change hart color

--- CockDive/CockDive/View/ViewComponent/LikeButtonView.swift
+++ CockDive/CockDive/View/ViewComponent/LikeButtonView.swift
@@ -67,6 +67,7 @@ struct Particle: Identifiable {
     var opacity: Double
     var endX: CGFloat
     var endY: CGFloat
+    var color: Color
 }
 
 struct ParticleEffectView: View {
@@ -82,7 +83,7 @@ struct ParticleEffectView: View {
                     .resizable()
                     .scaledToFit()
                     .frame(width: particleSize.width / 3, height: particleSize.height / 3) // 大きさを調整
-                    .foregroundColor(.pink)
+                    .foregroundColor(particle.color)
                     .scaleEffect(particle.scale)
                     .position(x: particle.x, y: particle.y)
                     .opacity(particle.opacity)
@@ -112,13 +113,15 @@ struct ParticleEffectView: View {
         particles = (0..<particleCount).map { _ in
             let angle = Double.random(in: (.pi / 2)...(.pi)) // 上方向への角度を設定
             let distance = CGFloat.random(in: buttonSize.width...buttonSize.width*2)
+            let hue = Double.random(in: 0...1) // 0から1の間でランダムに色相を決定
             return Particle(
                 x: buttonSize.width / 2,  // 初期位置を中心に設定
                 y: buttonSize.height / 2,  // 初期位置を中心に設定
                 scale: CGFloat.random(in: 0.5...1.0),
                 opacity: Double.random(in: 0.5...1.0),
                 endX: buttonSize.width / 2 + distance * cos(angle),  // ランダムな方向へ
-                endY: buttonSize.height / 2 - distance * sin(angle)   // 上方向へ移動
+                endY: buttonSize.height / 2 - distance * sin(angle),   // 上方向へ移動
+                color: Color(hue: hue, saturation: 1, brightness: 1) // 色相を使用して虹色を生成
             )
         }
     }
