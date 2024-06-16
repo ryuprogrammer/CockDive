addf3b9a29adb8b35b671043c2fffaa343fc84c6
トム・クルーズ <ryu1421magic@icloud.com>
2024-06-16 15:20:04 +0900

    create advertisementBarView

---
+++ CockDive/CockDive/View/AdvertisementViews/AdvertisementBarView.swift
@@ -0,0 +1,20 @@
+//
+//  AdvertisementBarView.swift
+//  CockDive
+//
+//  Created by トム・クルーズ on 2024/06/16.
+//
+
+import SwiftUI
+
+struct AdvertisementBarView: View {
+    var body: some View {
+        VStack {
+            Text("カレンダーを埋め尽くそう！")
+        }
+    }
+}
+
+#Preview {
+    AdvertisementBarView()
+}
