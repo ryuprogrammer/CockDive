6357492b14d1c0670ae92e6671c7d0ccce867fa7
トム・クルーズ <ryu1421magic@icloud.com>
2024-06-16 15:44:26 +0900

    animation Color

--- CockDive/CockDive/View/AdvertisementViews/AdvertisementBarView.swift
+++ CockDive/CockDive/View/AdvertisementViews/AdvertisementBarView.swift
@@ -1,16 +1,42 @@
-//
-//  AdvertisementBarView.swift
-//  CockDive
-//
-//  Created by トム・クルーズ on 2024/06/16.
-//
-
 import SwiftUI
 
 struct AdvertisementBarView: View {
+    @State private var gradientColors: [Color] = [.blue, .blue, .green]
+
     var body: some View {
-        VStack {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: gradientColors),
+                startPoint: .leading,
+                endPoint: .trailing
+            )
+            .opacity(0.5)
+            .frame(maxWidth: .infinity)
+            .frame(height: 70)
+            .onAppear {
+                animateColors()
+            }
+
             Text("カレンダーを埋め尽くそう！")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(Color.white)
+        }
+    }
+
+    private func animateColors() {
+        let baseColors: [[Color]] = [
+            [.blue, .blue, .green],
+            [.green, .blue, .blue, .green],
+            [.green, .blue, .blue]
+        ]
+
+        var index = 0
+        Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { timer in
+            withAnimation(.easeInOut(duration: 2.0)) {
+                index = (index + 1) % baseColors.count
+                gradientColors = baseColors[index]
+            }
         }
     }
 }
