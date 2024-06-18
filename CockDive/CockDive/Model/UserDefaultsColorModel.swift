10871f7f129df77e892573f9ac4e7f2ce37c2d0e
トム・クルーズ <ryu1421magic@icloud.com>
2024-06-18 14:49:18 +0900

    create Color Model

---
+++ CockDive/CockDive/Model/UserDefaultsColorModel.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct UserDefaultsColorModel {
+    var colorKey: String = "colorKey"
+
+    /// カラーを保存
+    func saveColor(color: Color) {
+        let uiColor = UIColor(color)
+        if let colorData = try? NSKeyedArchiver.archivedData(withRootObject: uiColor, requiringSecureCoding: false) {
+            UserDefaults.standard.set(colorData, forKey: colorKey)
+        }
+    }
+
+    /// カラーを取得
+    func fetchColor() -> Color {
+        guard let colorData = UserDefaults.standard.data(forKey: colorKey),
+              let uiColor = try? NSKeyedUnarchiver.unarchivedObject(ofClass: UIColor.self, from: colorData) else {
+            return .white // デフォルトの色
+        }
+        return Color(uiColor)
+    }
+}
