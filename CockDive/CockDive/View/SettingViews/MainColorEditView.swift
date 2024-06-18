94028738b497ce301b75791dacc2f0987dba622a
トム・クルーズ <ryu1421magic@icloud.com>
2024-06-19 03:45:02 +0900

    add comment

--- CockDive/CockDive/View/SettingViews/MainColorEditView.swift
+++ CockDive/CockDive/View/SettingViews/MainColorEditView.swift
@@ -4,48 +4,57 @@ struct MainColorEditView: View {
     // ルート階層から受け取った配列パスの参照
     @Binding var path: [CockCardNavigationPath]
     @State private var selectedColor: Color = .mainColor
-    let sampleColors: [Color] = [.red, .green, .blue, .yellow, .orange, .purple, .pink]
+    @State private var showError: Bool = false
+    let sampleColors: [Color] = [
+        .red, .green, .blue, .yellow, .orange, .purple, .pink, .brown,
+        .cyan, .teal, .indigo, .mint
+    ]
     let mainColorEditViewModel = MainColorEditViewModel()
+
+    let window = UIApplication.shared.connectedScenes.first as? UIWindowScene
+    var circleSize: CGFloat {
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let screen = windowScene.windows.first?.screen {
+            return screen.bounds.width / 7
+        }
+        return 50
+    }
 
     var body: some View {
         VStack {
             Spacer()
                 .frame(height: 50)
 
-            // ColorPicker
-            Text("細かい色を選択")
-                .font(.headline)
-                .padding(.bottom, 5)
-            ColorPicker("カラーパレットから細かく設定", selection: $selectedColor)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(10)
-                .shadow(radius: 2)
-                .padding(.bottom, 20)
-
             // Sample colors
             Text("おすすめの色")
                 .font(.headline)
                 .padding(.bottom, 5)
 
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
+            ScrollView {
+                LazyVGrid(columns: Array(repeating: .init(.flexible()), count: 4), spacing: 10) {
                     ForEach(sampleColors, id: \.self) { color in
                         Button(action: {
-                            selectedColor = color
+                            if color != .white && color != .black {
+                                selectedColor = color
+                                showError = false
+                            } else {
+                                showError = true
+                            }
                         }) {
                             Circle()
                                 .fill(color)
-                                .frame(width: 40, height: 40)
-                                .overlay(Circle().stroke(Color.white, lineWidth: selectedColor == color ? 2 : 0))
-                                .shadow(radius: selectedColor == color ? 1 : 0)
+                                .frame(width: circleSize, height: circleSize)
+                                .overlay(Circle().stroke(Color.white, lineWidth: selectedColor == color ? 3 : 0))
+                                .shadow(
+                                    color: selectedColor == color ? color : .clear,
+                                    radius: selectedColor == color ? 5 : 0
+                                )
                                 .padding(1)
                         }
                     }
                 }
-                .padding(.horizontal)
+                .padding()
             }
-            .padding(.bottom, 20)
 
             // Display selected colors
             Text("選択された色")
@@ -56,14 +65,17 @@ struct MainColorEditView: View {
                 .frame(width: 100, height: 100)
                 .cornerRadius(10)
                 .shadow(radius: 5)
-                .padding(.bottom, 20)
 
             Spacer()
+                .frame(height: 100)
 
+            Text("テーマカラーはすぐに更新されない場合があります。")
             // Confirm button
             Button(action: {
-                mainColorEditViewModel.setMainColor(color: selectedColor)
-                path.removeLast()
+                if selectedColor != .white && selectedColor != .black {
+                    mainColorEditViewModel.setMainColor(color: selectedColor)
+                    path.removeLast()
+                }
             }) {
                 StrokeButtonUI(text: "決定", size: .large, isFill: false)
             }
