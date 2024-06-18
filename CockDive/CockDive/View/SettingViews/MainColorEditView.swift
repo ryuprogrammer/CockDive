5f9d8d0556c18ea9f388043462144d60aed4f11b
トム・クルーズ <ryu1421magic@icloud.com>
2024-06-18 14:44:28 +0900

    create SelectColor

--- CockDive/CockDive/View/SettingViews/MainColorEditView.swift
+++ CockDive/CockDive/View/SettingViews/MainColorEditView.swift
@@ -1,11 +1,62 @@
 import SwiftUI
 
 struct MainColorEditView: View {
+    @State private var selectedColor: Color = .mainColor
+    let sampleColors: [Color] = [.red, .green, .blue, .yellow, .orange, .purple, .pink]
+
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Spacer()
+                .frame(height: 100)
+            // ColorPicker
+            ColorPicker("Pickerから細かく設定", selection: $selectedColor)
+                .padding()
+
+            // Sample colors
+            Text("おすすめの色")
+                .padding(.top)
+
+            HStack {
+                ForEach(sampleColors, id: \.self) { color in
+                    Button(action: {
+                        selectedColor = color
+                    }) {
+                        Circle()
+                            .fill(color)
+                            .frame(width: 40, height: 40)
+                    }
+                }
+            }
+            .padding()
+
+            // Display selected color
+            Rectangle()
+                .fill(selectedColor)
+                .frame(width: 100, height: 100)
+                .padding()
+
+            Spacer()
+
+            // Confirm button
+            Button(action: {
+                print("Selected color confirmed: \(selectedColor.description)")
+                // ここに決定ボタンが押された時のアクションを追加
+            }) {
+                StrokeButtonUI(text: "決定", size: .large, isFill: false)
+            }
+            .padding(.top)
+        }
+        .padding()
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationTitle("テーマカラーを変更")
+        .toolbarColorScheme(.dark)
+        .toolbarBackground(Color.mainColor, for: .navigationBar)
+        .toolbarBackground(.visible, for: .navigationBar)
     }
 }
 
 #Preview {
-    MainColorEditView()
+    NavigationStack {
+        MainColorEditView()
+    }
 }
